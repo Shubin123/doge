4219d2681f53f32cee19abc6f4d1a7aecaff7a2c
Shubin <wangshubin130@gmail.com>
2025-05-24 02:11:08 -0700

    add build commands

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
 cd ./src && zip -9 -r ../game.love .
+cd ..
+cp -r /Applications/love.app/ ./game.app
+cp  ./game.love ./game.app/Contents/Resources/
+
+# for windows cross-compile (get the love.exe (32/64)bit):
+# cat love.exe game.love > game.exe 
