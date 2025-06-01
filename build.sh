4d945ce9d5d718780600721cf07a163beed9f175
Shubin <wangshubin130@gmail.com>
2025-06-01 14:10:25 -0700

    move miscellaneousitems to ./misc directory aswell as builds to said directory

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-cd ./src && zip -9 -r ../game.love .
+cd ./src && zip -9 -r ../misc/game.love .
 cd ..
-cp -r /Applications/love.app/ ./game.app
-cp  ./game.love ./game.app/Contents/Resources/
+cp -r /Applications/love.app/ ./misc/game.app
+cp  ./misc/game.love ./misc/game.app/Contents/Resources/
 
 # for windows cross-compile (get the love.exe (32/64)bit):
-cd love-11.5-win64
+cd ./misc/love-11.5-win64
 cat love.exe ../game.love > ./game.exe 
