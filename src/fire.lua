6a6dc3ac869ed10504fe3d604e4a3b02956f947a
Shubin <wangshubin130@gmail.com>
2025-06-03 20:30:29 -0700

    fix sorting for networked fire and players

--- src/fire.lua
+++ src/fire.lua
@@ -4,14 +4,13 @@ fire.t = 0
 fire.fireables = {}
 fire.count = 10
 fire.pierce = true
-fire_bodies = {} -- only have collision when they are shot, not spinning (maybe change?)
+fire_bodies = {}    -- only have collision when they are shot, not spinning (maybe change?)
 fire_instances = {} -- no collision on these for now
 -- local sprite = require('sprite')
 
 fireSpriteImg = love.graphics.newImage('gfx/firelowres.png')
 -- myMath = require("myMath")
 function fire.load()
-
     Quads = sprite:constructsprite(fireSpriteImg, 8, 8)
     fire.particleSystem = love.graphics.newParticleSystem(fireSpriteImg, 500)
     particleSystem = fire.particleSystem
@@ -30,22 +29,24 @@ function fire.load()
     fire.particleSystem:setRotation(0, 2 * 3.14)
     fire.particleSystem:setOffset(sprite:getTileSize())
     fire.particleSystem:setInsertMode('bottom')
+
+    for i = 1, fire.count do
+        table.insert(fire_instances, vec2.new(0, 0))
+    end
 end
 
 function fire.update(dt)
     fire.particleSystem:update(dt)
     fire.t = fire.t + dt
-
-
 end
-function fire.draw()
 
+function fire.draw()
     -- love.graphics.setBlendMode("add")
     love.graphics.setBlendMode("lighten", "premultiplied")
 
     -- love.graphics.setColor(.90, .17, .48, 1)
     love.graphics.setColor(.13, .37, 1, 1)
-    
+
     fire.fires(fire.count)
     love.graphics.setColor(1, 1, 1)
     love.graphics.draw(fire.particleSystem, 500, 200, 0, fire.scale, fire.scale)
@@ -62,104 +63,116 @@ function fire.draw()
 end
 
 function fire.fires(n)
-    
-
     for i = 1, n do
         -- love.graphics.setColor(1,1,1, 1)
         -- love.graphics.draw(fire.particleSystem, player.body:getX() + math.sin(fire.t * 5 + 30 + 10 * i) * 20 - 35, player.body:getY() + math.cos(fire.t * 5 + 30 + 10 * i) * 20 - 30, 0, fire.scale, fire.scale, -2348, -808)
-        love.graphics.draw(fire.particleSystem, player.body:getX() + 200 + math.sin(fire.t * 5 + i) * 20, player.body:getY() + math.cos(fire.t * 5 + i) * 20 + 45, 0,fire.scale,fire.scale)
-
+        love.graphics.draw(fire.particleSystem, player.body:getX() + 200 + math.sin(fire.t * 5 + i) * 20,
+            player.body:getY() + math.cos(fire.t * 5 + i) * 20 + 45, 0, fire.scale, fire.scale)
     end
 end
-
 
 function fire.populate()
     -- Main static fire
-        local fire_main_x, fire_main_y = 500, 200
-        table.insert(dynamic_draw_list, {
-            sort_y = fire_main_y,
-            image_or_particles = fire.particleSystem,
-            quad = nil,
-            x = fire_main_x,
-            y = fire_main_y,
-            rotation = 0,
-            scale_x = fire.scale,
-            scale_y = fire.scale,
-            offset_x = 0,
-            offset_y = 0,
-            color = {0.13, 0.37, 1, 1},
-            blend_mode = {"lighten", "premultiplied"},
-            source_object_type = "fire_effect"
-        })
-        
-for i = 1, fire.count do
-    fire_instance_x = player.body:getX() + (math.sin(fire.t * 1 + i)*(math.sin(fire.t*2) + 2)) * 30 + 200
-    fire_instance_y = player.body:getY() + (math.cos(fire.t * 1 + i)*(math.sin(fire.t*2)+ 2))* 30 + 45
-    
+    local fire_main_x, fire_main_y = 500, 200
+    table.insert(dynamic_draw_list, {
+        sort_y = fire_main_y,
+        image_or_particles = fire.particleSystem,
+        quad = nil,
+        x = fire_main_x,
+        y = fire_main_y,
+        rotation = 0,
+        scale_x = fire.scale,
+        scale_y = fire.scale,
+        offset_x = 0,
+        offset_y = 0,
+        color = { 0.13, 0.37, 1, 1 },
+        blend_mode = { "lighten", "premultiplied" },
+        source_object_type = "fire_effect"
+    })
 
-    if i <= #fire.fireables then
-        if not fire.fireables[i][3] then
-            -- Set starting position
-            fire.fireables[i][1] = vec2.new(fire_instance_x, fire_instance_y)
-            
-            -- print(vec2.norm(fire.fireables[i][1]))
-            local _bod = love.physics.newBody(world, fire_instance_x - 200, fire_instance_y -45,"dynamic")
-            
-            table.insert(fire_bodies , i, _bod)
-            local _fixture = love.physics.newFixture(_bod, love.physics.newCircleShape(20))
-            _fixture:setGroupIndex(-1)
-            -- _fixture:setFilterData(500,1, -1)
-            -- _bod:applyForce(fire.fireables[i][2].x *fire.t,fire.fireables[i][2].y*fire.t)
-            fire.fireables[i][3] = 0  -- initialized
-            -- direction is already stored in [2]
-        else
-            -- Move fireball using the pre-calculated direction
-            -- print(fire_bodies[1]:getPosition())
-            -- local tmp_fire = fire.fireables[i][1]
-            fire.fireables[i][1] = fire.fireables[i][1] + fire.fireables[i][2]*2
-            -- love.graphics.line(fire.fireables[i][1].x - 200, fire.fireables[i][1].y - 45, (tmp_fire.x - 200)*2, (tmp_fire.y - 45)*2) -- debug draw
-            fire_bodies[i]:setPosition(fire.fireables[i][1].x - 200, fire.fireables[i][1].y - 45)
-        end
+    for i, fire_instance in pairs(fire_instances) do
+    -- local fire_instance_x = player.body:getX() + (math.sin(fire.t * 1 + i) * (math.sin(fire.t * 2) + 2)) * 30 + 200
+    -- local fire_instance_y = player.body:getY() + (math.cos(fire.t * 1 + i) * (math.sin(fire.t * 2) + 2)) * 30 + 45
+    
+    -- fire_instance.x = player.body:getX() + (math.sin(fire.t * 1 + i) * (math.sin(fire.t * 2) + 2)) * 30 + 200
+    fire_instance.x = player.body:getX() + 10
+    
+    -- fire_instance.y = player.body:getY() + (math.cos(fire.t * 1 + i) * (math.sin(fire.t * 2) + 2)) * 30 + 45
+    fire_instance.y = player.body:getY() + 10
+    -- table.insert(fire_instances, {fire_instance_x, fire_instance_y})
+    
+    -- Default fire effect
+    table.insert(dynamic_draw_list, {
+        sort_y = fire_instance.y + 100,
+        image_or_particles = fire.particleSystem,
+        quad = nil,
+        x = fire_instance.x,
+        y = fire_instance.y,
+        rotation = 0,
+        scale_x = fire.scale,
+        scale_y = fire.scale,
+        offset_x = 250,
+        offset_y = 50,
+        color = { 1, 1, 1, 1 },
+        blend_mode = { "lighten", "premultiplied" },
+        source_object_type = "fire_effect"
+    })
+end
+
+-- Fireables loop using pairs
+for i, fireable in pairs(fire.fireables) do
+    if fireable[1] then
+    if not fireable[3] then
         
-        -- Draw fireball
-        table.insert(dynamic_draw_list, {
-            sort_y = fire.fireables[i][1].y + 100,
-            image_or_particles = fire.particleSystem,
-            quad = nil,
-            x = fire.fireables[i][1].x,
-            y = fire.fireables[i][1].y,
-            rotation = 0,
-            scale_x = fire.scale,
-            scale_y = fire.scale,
-            offset_x = 250,
-            offset_y = 50,
-            color = {1, 1, 1, 1},
-            blend_mode = {"lighten", "premultiplied"},
-            source_object_type = "fire_effect"
-        })
+        -- Set starting position
+        -- fireable[1] = vec2.new(fire_instances[i].x, fire_instances[i].y)
+        
+        -- print(vec2.norm(fireable[1]))
+        local _bod = love.physics.newBody(world, fireable[1].x - 200, fireable[1].y - 45, "dynamic")
+        
+        table.insert(fire_bodies, i, _bod)
+        local _fixture = love.physics.newFixture(_bod, love.physics.newCircleShape(20))
+        _fixture:setGroupIndex(-1)
+        -- _fixture:setFilterData(500,1, -1)
+        -- _bod:applyForce(fireable[2].x *fire.t,fireable[2].y*fire.t)
+        fireable[3] = 1 -- initialized
+        -- direction is already stored in [2]
+
+
     else
-        -- Default fire effect
-        table.insert(dynamic_draw_list, {
-            sort_y = fire_instance_y + 100,
-            image_or_particles = fire.particleSystem,
-            quad = nil,
-            x = fire_instance_x,
-            y = fire_instance_y,
-            rotation = 0,
-            scale_x = fire.scale,
-            scale_y = fire.scale,
-            offset_x = 250,
-            offset_y = 50,
-            color = {1, 1, 1, 1},
-            blend_mode = {"lighten", "premultiplied"},
-            source_object_type = "fire_effect"
-        })
+        -- Move fireball using the pre-calculated direction
+        -- print(fire_bodies[1]:getPosition())
+        -- local tmp_fire = fireable[1]
+        fireable[1] = fireable[1] + fireable[2] * 2
+        -- love.graphics.line(fireable[1].x - 200, fireable[1].y - 45, (tmp_fire.x - 200)*2, (tmp_fire.y - 45)*2) -- debug draw
+        fire_bodies[i]:setPosition(fireable[1].x - 200, fireable[1].y - 45)
     end
     
+    -- Draw fireball
+    table.insert(dynamic_draw_list, {
+        sort_y = fireable[1].y + 100,
+        image_or_particles = fire.particleSystem,
+        quad = nil,
+        x = fireable[1].x,
+        y = fireable[1].y,
+        rotation = 0,
+        scale_x = fire.scale,
+        scale_y = fire.scale,
+        offset_x = 250,
+        offset_y = 50,
+        color = { 1, 1, 1, 1 },
+        blend_mode = { "lighten", "premultiplied" },
+        source_object_type = "fire_effect"
+    })
 end
+end
+    
 end
 
-function fire.collision(fixture_a,fixture_b, contact)
+function fire.collision(fixture_a, fixture_b, contact)
+    if not (var.multiplayer == 2) then
+
+
     local not_fire
     local firef
     if fixture_a:getGroupIndex() == -1 then
@@ -172,29 +185,26 @@ function fire.collision(fixture_a,fixture_b, contact)
 
     -- print(fixture_a:getGroupIndex() == -1 , fixture_b:getGroupIndex() == -1)
     -- print(math.random() < 0.01 and 1 or 0)
-    if  not_fire ~= nil then
-    -- print(not_fire:getGroupIndex())
-    if (checkDestroy(enemies_bods, not_fire:getBody())) then 
-        fire.count = fire.count + (math.random() < 0.1 and 1 or 0)
-    end
-    if (checkDestroy(coin_bods, not_fire:getBody())) then
-        fire.count = fire.count + (math.random() < 0.1 and 1 or 0)
-    end
+    if not_fire ~= nil then
+        -- print(not_fire:getGroupIndex())
+        if (checkDestroy(enemies_bods, not_fire:getBody())) then
+            fire.count = fire.count + (math.random() < 0.1 and 1  or 0)
+        end
+        if (checkDestroy(coin_bods, not_fire:getBody())) then
+            fire.count = fire.count + (math.random() < 0.1 and 1 or 0)
+        end
         if not fire.pierce then
-        table.remove(fire.fireables,checkDestroy(fire_bodies, firef:getBody()) or 0) -- remove line for piercing !!
+            table.remove(fire.fireables, checkDestroy(fire_bodies, firef:getBody()) or 0) -- remove line for piercing !!
         end
         -- checkDestroy(fire_bodies, firef:getBody())
-    -- print()
-    
+        -- print()
     end
-    
-                                                                                                                   
 end
-
+end
 
 function fire.getNetworkData()
     local network_data = {}
-    
+
     -- Include main fire system state
     -- -- network_data.main_fire = {
     --     t = fire.t,
@@ -202,15 +212,17 @@ function fire.getNetworkData()
     --     scale = fire.scale,
     --     active = true
     -- }
-    
+
     -- Include fireball projectiles data
     -- network_data.fireables = {}
     for i = 1, #fire.fireables do
         if fire.fireables[i] and fire.fireables[i][3] ~= nil then -- initialized fireball
+        -- print(fire.fireables[i][1].x)
+        if (fire.fireables[i][1]) then
             table.insert(network_data, {
                 -- position = {
-                    x = fire.fireables[i][1].x,
-                    y = fire.fireables[i][1].y,
+                x = fire.fireables[i][1].x,
+                y = fire.fireables[i][1].y,
                 -- },
                 -- direction = {
                 --     x = fire.fireables[i][2].x,
@@ -220,27 +232,28 @@ function fire.getNetworkData()
                 active = true
             })
         end
+        end
     end
-    
+
     -- Include fire bodies physics data (for collision sync)
     -- network_data.fire_bodies = {}
-    
-    local spinning_fires = fire.count - #fire.fireables
-    for i = 1, spinning_fires do
-        local fire_instance_x = player.body:getX() + (math.sin(fire.t * 1 + i)*(math.sin(fire.t*2) + 2)) * 30 + 200
-        local fire_instance_y = player.body:getY() + (math.cos(fire.t * 1 + i)*(math.sin(fire.t*2)+ 2))* 30 + 45
-        
-        local index = #fire.fireables + i
+    -- print(fire_instances)
+    -- for k,v in pairs(fire_instances) do print(k,v.x) end
+
+
+    for i = 1, #fire_instances do
+        -- local fire_instance_x = player.body:getX() + (math.sin(fire.t * 1 + i)*(math.sin(fire.t*2) + 2)) * 30 + 200
+        -- local fire_instance_y = player.body:getY() + (math.cos(fire.t * 1 + i)*(math.sin(fire.t*2)+ 2))* 30 + 45
+        -- local index = #fire.fireables + i
         table.insert(network_data, {
-            x = fire_instance_x,
-            y = fire_instance_y,
+            x = fire_instances[i].x,
+            y = fire_instances[i].y,
             active = true
             -- type = "spinning"
         })
     end
-    
+
     return network_data
 end
-
 
 return fire
