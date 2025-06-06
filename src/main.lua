6724b717ba100c6941d19cc9bffebb78a51372d9
Shubin <wangshubin130@gmail.com>
2025-06-05 17:56:14 -0700

    revert ffi
    using c structs to pack the data did not make it anymore efficient. more testing needed.

--- src/main.lua
+++ src/main.lua
@@ -280,7 +280,7 @@ local zoomToggle = false;
 function love.keypressed(key)
     if key == "z" then
         if not zoomToggle then
-            camera.setZoom(0.3)
+            camera.setZoom(2)
             -- player.body:applyForce(1000,0)
         else
             camera.setZoom(1)
