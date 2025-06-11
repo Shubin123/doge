bff54ff91320316e452b70ac90a7b39b6833f879
Shubin <wangshubin130@gmail.com>
2025-06-11 00:43:21 -0700

    Revert "async weather api"
    This reverts commit 76f37b63188f5627e876c44d60f3bb22b081fa13.

--- src/main.lua
+++ src/main.lua
@@ -171,7 +171,7 @@ function love.draw()
     shader.prepass()
     end
 
-    if moonshine.isOn then 
+    -- if moonshine then 
          
       blueNeon(function()
     love.graphics.setColor(0.17, 0.46, 1)
@@ -194,7 +194,7 @@ function love.draw()
         
     love.graphics.setColor(1,1,1,1)
     end)
-    end 
+    -- end 
     
    
     love.graphics.push() --push all camera transforms (move everything when player moves)
